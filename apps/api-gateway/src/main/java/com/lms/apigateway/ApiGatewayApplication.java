350c27954499e79a96614b347a07e3686773bc80
Noman5237 <anonyman637@gmail.com>
2023-03-16 00:24:15 +0600

    Dynamic API Gateway bug fixed, health checks added and all services orchestrated

--- apps/api-gateway/src/main/java/com/lms/apigateway/ApiGatewayApplication.java
+++ apps/api-gateway/src/main/java/com/lms/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,20 @@
 package com.lms.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 	
+	@Value("${hosts.user-management-service}")
+	private String userManagementServiceHost;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+		ApiGatewayApplication app = new ApiGatewayApplication();
+		System.out.println("===================================");
+		System.out.println(app.userManagementServiceHost);
 	}
 	
 }
