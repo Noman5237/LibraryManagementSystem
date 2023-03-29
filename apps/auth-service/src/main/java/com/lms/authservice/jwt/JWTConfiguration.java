89cd247e0b6421fa693dafbf00fdea35cc3a7f36
Noman5237 <anonyman637@gmail.com>
2023-03-29 20:10:13 +0600

    Added cloud config, doc and actuator to auth service

--- apps/auth-service/src/main/java/com/lms/authservice/jwt/JWTConfiguration.java
+++ apps/auth-service/src/main/java/com/lms/authservice/jwt/JWTConfiguration.java
@@ -2,12 +2,14 @@ package com.lms.authservice.jwt;
 
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
 @Configuration
+@RefreshScope
 public class JWTConfiguration {
 	
 	@Value ("${security.jwt.access-token.secret}")
