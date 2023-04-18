353d0b513db1df306b4f66ebac4418b3e3024a9e
Noman5237 <anonyman637@gmail.com>
2023-04-18 20:02:17 +0600

    redis connection factory configuration fixed

--- apps/auth-service/src/main/java/com/lms/authservice/authentication/config/RedisConfiguration.java
+++ apps/auth-service/src/main/java/com/lms/authservice/authentication/config/RedisConfiguration.java
@@ -3,21 +3,12 @@ package com.lms.authservice.authentication.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfiguration {
-	
-	@Bean
-	public RedisConnectionFactory connectionFactory() {
-		var configuration = new RedisStandaloneConfiguration();
-		configuration.setPassword("password@auth");
-		return new LettuceConnectionFactory(configuration);
-	}
 	
 	@Bean
 	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
